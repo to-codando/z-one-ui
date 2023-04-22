6a66e47c88207275525d7b8bfed106a19b22f525
marciely <marcytech@gmail.com>
2023-04-21 23:35:02 -0300

    test: includ fam-button component test

---
+++ src/components/FamButton/FamButton.spec.ts
@@ -0,0 +1,36 @@
+import { html, render } from "iares";
+import globalJsdom from "global-jsdom";
+// import * as sinon from "sinon";
+import { expect } from "@esm-bundle/chai";
+
+import { FamButton } from "@/components/FamButton";
+
+describe("FamButton", () => {
+  let cleanup: { (): void };
+
+  before(() => {
+    cleanup = globalJsdom();
+  });
+
+  after(() => {
+    cleanup();
+  });
+
+  it("Should ensure that the component has the correct properties", () => {
+    expect(FamButton()).to.have.all.keys(["template", "styles"]);
+  });
+
+  it("Should render button content correctly", () => {
+    render(html`
+       <${FamButton}>
+          <slot target="content">
+            <span style="tertiary:disabled">
+              Salvar
+            </span>
+          </slot>
+        </>
+    `);
+    const element = document.querySelector("fam-button");
+    expect(element?.firstChild?.textContent).to.equal("Salvar");
+  });
+});
