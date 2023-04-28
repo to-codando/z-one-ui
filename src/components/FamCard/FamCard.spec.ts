d7e7d424ae3591e42f0fb6fd2bda7fb19dbbd465
marciely <marcytech@gmail.com>
2023-04-27 21:12:27 -0300

    test: includes FamCard test

---
+++ src/components/FamCard/FamCard.spec.ts
@@ -0,0 +1,24 @@
+import globalJsdom from "global-jsdom";
+// import * as sinon from "sinon";
+import { expect } from "@esm-bundle/chai";
+import { render, html } from "iares";
+
+import { FamCard } from "@/components/FamCard";
+
+describe("AppTitle", () => {
+  let cleanup: { (): void };
+  let component = FamCard();
+
+  before(() => {
+    cleanup = globalJsdom();
+    component = FamCard();
+  });
+
+  after(() => {
+    cleanup();
+  });
+
+  it("Should have template and style defined", () => {
+    expect(component).to.have.all.keys(["template", "styles"]);
+  });
+});
